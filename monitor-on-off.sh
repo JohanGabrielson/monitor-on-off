6b0013cd1327faf0180232b4c1626a059b3e87c0
Johan <johan.gabrielson@gmail.com>
2026-02-28 23:30:28 +0100

    Added function to end script if adapter is not detected

--- monitor-on-off.sh
+++ monitor-on-off.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  Monitor Mode
+#  Script to switch on/off for monitor mode
 #  by Johan 
 
 
@@ -25,10 +25,24 @@ IFACE="wlan0"
 MONITOR="wlan0mon"
 
 
+# -------------------------------
+# Adapter check
+# -------------------------------
+if ! ip link show "$IFACE" >/dev/null 2>&1 && ! ip link show "$MONITOR" >/dev/null 2>&1; then
+
+    echo -e "${RED} Interface '$IFACE' not detected.${RESET}"
+    echo -e "${YELLOW} Connect adapter and try again.${RESET}"
+    exit 1
+fi
+
+echo -e "${GREEN}  Adapter found: $IFACE${RESET}"
+# -------------------------------
+
+
 
 # Detect if monitor mode is active
 if iwconfig 2>/dev/null | grep -q "$MONITOR"; then
-    echo -e "${RED}[*] Monitor mode detected. Switching back to managed mode...${RESET}"
+    echo -e "${CYAN}[*] Monitor mode detected. Switching back to managed mode...${RESET}"
     sudo airmon-ng stop $MONITOR >/dev/null 2>&1
     sudo systemctl restart NetworkManager
     echo -e "${GREEN}[+] Adapter is now back in normal mode.${RESET}"
